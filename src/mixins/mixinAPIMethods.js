67d420fb9b08c47f8d7b48e199a985dfab952b8b
bedomarci <bedomarci@gmail.com>
2022-07-23 12:30:44 +0200

    .

--- src/mixins/mixinAPIMethods.js
+++ src/mixins/mixinAPIMethods.js
@@ -68,6 +68,7 @@ export default {
         .catch(this.apiError)
     },
     createCard(model) {
+      delay(1000);
       return axios.post(
         c.API_CARD_CREATE_URL,
         { ...model.create },
@@ -75,6 +76,7 @@ export default {
       )
     },
     updateCard(cardID, updateModel) {
+      delay(1000);
       return axios.put(
         c.API_CARD_UPDATE_URL.replace('{CardId}', cardID),
         { ...updateModel },
@@ -88,6 +90,7 @@ export default {
       )
     },
     updateGroups(cardID, updateGroups) {
+      delay(1000);
       return axios.put(
         c.API_CARD_GROUPS_URL.replace('{CardId}', cardID),
         updateGroups,
